a584eb763592d4c27ed84b8e1c6e7f4ca3fdc183
Vivekmasona <112853103+Vivekmasona@users.noreply.github.com>
2023-09-21 21:24:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,33 +1,34 @@
-
 const express = require('express');
 const ytdl = require('ytdl-core');
+const fs = require('fs');
 const app = express();
 const port = 3000;
 
-app.get("/download", async (req, res) => {
-  const url = req.query.url;
-  const type = "mp3"; // Set the type to "mp3" for audio download
-
+app.get('/download', async (req, res) => {
   try {
-    const info = await ytdl.getInfo(url);
-    const title = info.videoDetails.title;
+    const videoURL = req.query.url; // Get the YouTube video URL from the query parameter
 
-    // Find the MP3 format directly
-    const mp3Format = info.formats.find(format => format.container === 'mp3');
-
-    if (!mp3Format) {
-      res.status(404).send("MP3 format not available for this video.");
-      return;
+    if (!videoURL) {
+      return res.status(400).send('Missing video URL');
     }
 
-    res.header("Content-Disposition", `attachment; filename="${title}.${type}"`);
-    ytdl(url, { format: mp3Format }).pipe(res); // Download audio in MP3 format
-  } catch (err) {
-    console.log(err);
-    res.status(500).send("An error occurred.");
+    // Get information about the video (including title)
+    const info = await ytdl.getInfo(videoURL);
+    const title = info.videoDetails.title; // Get the video's title
+
+    // Set response headers to specify a downloadable file with the video's title
+    res.setHeader('Content-Disposition', `attachment; filename="${title}.mp3"`);
+    res.setHeader('Content-Type', 'audio/mpeg');
+
+    // Pipe the audio stream into the response
+    ytdl(videoURL, { quality: 'highestaudio' }).pipe(res);
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).send('Internal Server Error');
   }
 });
 
 app.listen(port, () => {
-  console.log("Server is running...");
+  console.log(`Server is running on port ${port}`);
 });
+  
